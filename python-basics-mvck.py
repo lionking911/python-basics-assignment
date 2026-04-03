c7ab8b65e44c2843e170b6d26953d82005306c4c
lionking911 <veeravoid@gmail.com>
2026-04-03 14:41:37 +0530

    changes of first assignment

--- python-basics-mvck.py
+++ python-basics-mvck.py
@@ -1,1 +1,14 @@
-this is
+name=input("Enter the Name")
+age=int(input("Enter ur age"))
+height=float(input("Enter ur height"))
+student=int(input(" if your student enter 1 else 0"))
+print("Name : ",name,"| AGE : ",age,"| Height : ",height,": Student : ", "is_student" if student==1 else "not student")
+age_in_months=age*12
+print("age in months : ",age_in_months)
+age_in_days=age*365
+print("age in days : ",age_in_days)
+
+reminde=age%7
+print("reminder when age is divided by 7 : ",reminde)
+age_power=age**2
+print("age raised to the power 2 : ",age_power)
